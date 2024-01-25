e751e0116f9474ea59f3b6711afcd4d9e768c583
justShox <147226861+justShox@users.noreply.github.com>
2024-01-25 21:42:35 +0500

    Made some changes with for

--- E_shop/index/urls.py
+++ E_shop/index/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('about-us', views.about),
     path('contact-us', views.contact),
     path('search', views.search_product),
+    path('category/<int:pk>', views.get_full_category),
+    path('product/<int:pk>', views.get_full_product),
+    path('product_not_found', views.pr_not_found),
 ]
--- E_shop/index/views.py
+++ E_shop/index/views.py
@@ -1,14 +1,39 @@
-from django.shortcuts import render
-from . import forms
+from django.shortcuts import render, redirect
+from . import forms, models
+from .models import Product, Category, Cart
 
 
 # Create your views here.
 # Отображение главной страницы
 def home(request):
+    # Поисковая строка
     search_bar = forms.SearchForm()
+    # Собираем все продукты
+    product_info = Product.objects.all()
+    # Собираем все категории товаров
+    category_info = Category.objects.all()
     # Отправить элементы на фронт
-    context = {'form': search_bar}
+    context = {'form': search_bar,
+               'product': product_info,
+               'category': category_info}
     return render(request, 'home.html', context)
+
+
+# Вывод товаров по категории
+def get_full_category(request, pk):
+    category = Category.objects.get(id=pk)
+    products = Product.objects.filter(category_name=category)
+    # Отправить данные на фронт
+    context = {'products': products}
+    return render(request, 'category.html', context)
+
+
+# Вывод товаров о конкретном продукте
+def get_full_product(request, pk):
+    product = Product.objects.get(id=pk)
+    # Отправить данные на фронт
+    context = {'product': product}
+    return render(request, 'product.html', context)
 
 
 # Отображение страницы о нас
@@ -24,4 +49,13 @@ def contact(request):
 # Поиск продуктов
 def search_product(request):
     if request.method == 'POST':
-        pass
+        get_product = request.post.get('search_product')
+        try:
+            exact_product = Product.objects.get(product_name__icontains=get_product)
+            return redirect(f'product/{exact_product.id}')
+        except:
+            return redirect('/pr-not-found')
+
+
+def pr_not_found(request):
+    return render(request, 'not_found.html')
